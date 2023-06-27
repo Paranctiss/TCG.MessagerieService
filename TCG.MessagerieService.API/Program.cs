c0cbd5e261607dcf40a7c0991f2a8649a9c12b93
Paranctiss <fli1@live.fr>
2023-06-27 19:03:17 +0200

    cors fix

--- TCG.MessagerieService.API/Program.cs
+++ TCG.MessagerieService.API/Program.cs
@@ -14,10 +14,11 @@ builder.Services.AddCors(options =>
                       builder =>
                       {
                           builder
-                            .WithOrigins("http://localhost:8100") // specifying the allowed origin
+                            //.WithOrigins("*") // specifying the allowed origin
                             .WithMethods("GET", "POST", "PUT", "DELETE") // defining the allowed HTTP method
                             .AllowAnyHeader()
-                            .AllowCredentials(); // allowing any header to be sent
+                            .AllowCredentials() // allowing any header to be sent
+                            .SetIsOriginAllowed((hosts) => true);
                       });
 });
 
